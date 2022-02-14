7777ec01fc3f4fb269f4f623296dcaa8463d4905
Artymanprince <adebiyiitunuayo@gmail.com>
2022-02-14 08:35:12 +0100

    solved

--- 10_names.js
+++ 10_names.js
@@ -7,3 +7,13 @@ const smart = ['Daudu', 'Emma', 'Skodo', 'John', 'Jones', 'Mike', 'Sierra', 'Osc
 for (const item of smart) {
     console.log(`${item}, ${item}`);
 }
+
+or
+
+const smart = ['Daudu', 'Emma', 'Skodo', 'John', 'Jones', 'Mike', 'Sierra', 'Oscar', 'Alpha', 'Bravo'];
+
+const newSmart = smart.map((item) => `${item}, ${item}`);
+//Creates a new array and does the same thing in the first code
+for (const item of newSmart) {
+    console.log(item);
+} 
