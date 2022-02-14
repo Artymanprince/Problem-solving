0b502cf7d37897c59171f5eb05697bf97e5ae721
Artymanprince <adebiyiitunuayo@gmail.com>
2022-02-14 08:32:30 +0100

    it

--- 10_names.js
+++ 10_names.js
@@ -2,3 +2,8 @@
 In this Array there are 10 names.
 Write a code to print each of these names Twice in <li> arrangement. */
 
+const smart = ['Daudu', 'Emma', 'Skodo', 'John', 'Jones', 'Mike', 'Sierra', 'Oscar', 'Alpha', 'Bravo'];
+
+for (const item of smart) {
+    console.log(`${item}, ${item}`);
+}
