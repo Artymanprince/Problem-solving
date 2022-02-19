b73153d1c27a579c9522416856834c5e9630aae5
Artymanprince <adebiyiitunuayo@gmail.com>
2022-02-19 23:36:24 +0100

    payment system

---
+++ Bank.js
@@ -0,0 +1,15 @@
+/*Daily Task:
+              Write a Program that performs the job of a payment System. Accept the Account number, Amount and displays 5 real Banks [only if account number is Submitted]. The User can then type in the bank he wants to use, and then Pay.*/
+var acct_num = prompt("Input your account number please.");
+acct_num = parseInt(acct_num, 10);
+const banks = ["UBA", "GtBank", "Access Bank", "Fidelity Bank", "Zenith Bank"];
+var amount;
+function process(){
+	console.log(banks);
+	prompt( "Please select from the list. 0-4", for (var i in banks) 
+{
+   console.log("row " + i) );
+	amount = prompt("Please input amount to be paid");
+	amount = parseInt(amount, 10);
+} 
+process();
